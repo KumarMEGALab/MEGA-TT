ad9522ba1131736eb84698d8ab9dd4ea85ed5e7c
glen stecher <tuf81013@temple.edu>
2022-04-28 13:50:52 -0700

    add missing file
    testconsts was missing from version control.

---
+++ tests/common/testconsts.pas
@@ -0,0 +1,19 @@
+unit testconsts;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils;
+
+const
+  //JSON_FILE = '/home/gstecher/Documents/lazarusProjects/mega6/MEGA6/CODEXE2/mega-tt/tests/resources/timeline_search.json';
+  JSON_FILE = '/home/gstecher/Documents/megatt/timelinesearch/inputs/felinae.json';
+  TIMELINE_SVG_NAME = 'Timeline';
+  IMPACTS_FILE = '/home/gstecher/Documents/megatt/timelinesearch/inputs/earth_impacts.csv';
+
+implementation
+
+end.
+
